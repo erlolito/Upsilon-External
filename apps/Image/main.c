6d8b907a2f7077f657694cbc4f8845ede0b72f91
shirAko <66240946+erlolito@users.noreply.github.com>
2024-11-05 23:01:00 +0100

    Update main.c

--- apps/Image/main.c
+++ apps/Image/main.c
@@ -1,13 +1,10 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "extapp_api.h"
 #include "img.h"
 
 void extapp_main(void) {
-while(extapp_scanKeyboard())extapp_msleep(10);
-for(unsigned int len=0;len<img_len;)extapp_pushRectUniform(len/480,(len/2)%240,1,1,img[len++]*256+img[len++]);
-int key=extapp_getKey(true,NULL);
-if(key==KEY_CTRL_EXIT||key==KEY_CTRL_MENU)return;
+    while(extapp_scanKeyboard())extapp_msleep(10);
+    extapp_pushRectUniform(0,0,320,240,0xffff);
+    for(unsigned int len=0;len<img_len;len++)extapp_pushRectUniform(len%320,len/320,1,1,img[len]);
+    int key=extapp_getKey(true,NULL);
+    if(key==KEY_CTRL_EXIT||key==KEY_CTRL_MENU)return;
 }
